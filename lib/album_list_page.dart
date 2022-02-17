c1b0b31b369e5e762d2ada6fd5dd5683ce607635
Yoh <yokeus2000@gmail.com>
2022-02-17 21:30:29 +0900

    change grid view appearance

--- lib/album_list_page.dart
+++ lib/album_list_page.dart
@@ -16,28 +16,41 @@ class _AlbumListPageState extends State<AlbumListPage> {
         title: const Text('List Page'),
       ),
       body: GridView.builder(
-          padding: const EdgeInsetsDirectional.all(20),
+          padding: const EdgeInsetsDirectional.all(12),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+              crossAxisCount: 2, crossAxisSpacing: 4, mainAxisSpacing: 4),
           itemCount: 16,
           itemBuilder: (BuildContext context, int index) {
-            return Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            // アルバム詳細ページに遷移
-                            builder: (context) => const AlbumAboutPage()));
-                  },
-                  child: const Image(
-                    fit: BoxFit.contain,
-                    image: NetworkImage(
-                        'https://src-radwimps.s3.ap-northeast-1.amazonaws.com/wp-content/uploads/2013/12/14142957/jk_xotsumi-1.jpg'),
+            return GestureDetector(
+              child: Card(
+                elevation: 2,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+                  child: Column(
+                    children: const [
+                      Image(
+                        fit: BoxFit.contain,
+                        image: NetworkImage(
+                            'https://src-radwimps.s3.ap-northeast-1.amazonaws.com/wp-content/uploads/2013/12/14142957/jk_xotsumi-1.jpg'),
+                      ),
+                      SizedBox(height: 4),
+                      Text('×と◯と罪と')
+                    ],
                   ),
                 ),
-              ],
+              ),
+              //画像をタップした時の動作
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        // アルバム詳細ページに遷移
+                        builder: (context) => const AlbumAboutPage()));
+              },
+              //画像を長押しした時の動作
+              onLongPress: () {
+                //ここに長押しした時の挙動を記述する
+              },
             );
           }),
     );
